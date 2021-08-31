2d14bbc375a1321e43f287f75d2e1b5cd3af6fd7
beerth <beerzii.4321@gmail.com>
2021-09-01 06:53:17 +0700

    deploy to heroku again edit cors

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ const path = require('path');
 dotenv.config();
 app.use(express.json());
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
